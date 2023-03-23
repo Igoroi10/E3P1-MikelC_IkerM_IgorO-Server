ade6c3bc9ed31d78ec0e99de0dae7470ff4531ce
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-03-23 18:42:23 +0100

    [UPDATE] adaptacion del verify a la tabla de la base de datos mythClash

--- routes/verifyUser.php
+++ routes/verifyUser.php
@@ -3,27 +3,27 @@
     require_once(__DIR__."/../functions.php");
     require_once (__DIR__."/../controller/Controller.php");
 
-    //Verificamos que se hayan enviado las variables 'name' y 'last_name' a trabés del formulario
+    //Verificamos que se hayan enviado las variables 'name' y 'pasahitza' a trabés del formulario
 
     //TEST OIST ROUTE QUITAR OISTERIORMENTE
-    // $_POST['name']          = 'Harry';
-    // $_POST['lastName']      = 'Smith';
+    // $_POST['emaila']          = 'iker.mendoza@ikasle.aeg.eus';
+    // $_POST['pasahitza']      = '$2y$10$kSL/Yle4VVBAQa8a0TypYeiJzNk8aqGAiy/7zHx30KELBYXyQy9vu';
 
 
-    if(isset($_POST['name']) && isset($_POST['lastName']))
+    if(isset($_POST['emaila']) && isset($_POST['pasahitza']))
     {
         // echo "entra en UserVerify";
-        $name           = sanitizeString($_POST['name']);
-        $last_name      = sanitizeString($_POST['lastName']);
+        $mail           = sanitizeString($_POST['emaila']);
+        $pasahitza      = sanitizeString($_POST['pasahitza']);
 
-        // echo $name;
-        // echo $last_name;
+        // echo $mail;
+        // echo $pasahitza;
 
-        $userSend['name']       = "";
-        $userSend['lastName']   = "";
+        $userSend['emaila']       = "";
+        $userSend['pasahitza']   = "";
         $userSend['error']      = "";
 
-        if ($name == "" ||$last_name == "")
+        if ($mail == "" ||$pasahitza == "")
         {
             //Error. No se ha insertado todos los campos
             $userSend['error'] = "Not all the fields were entered";
@@ -33,20 +33,20 @@
         {
             // echo "Entra en Else";
             //Buscamos el elemento de la tabla USERS
-            $resultArray = $user->getAllBy2Columns("name", $name, "last_name", $last_name);
+            $resultArray = $user->getAllBy2Columns("emaila", $mail, "pasahitza", $pasahitza);
 
             if($resultArray == null)
             {
                 // echo "entra if";
                 //Usuario no encontrado en la base de datos
-                $yserSend['error'] = "Invalid loggin attempt";
+                $userSend['error'] = "Invalid loggin attempt";
             }
             else
             {
                 // echo "entra else";
                 //Si el usuario esta en la base de datos lo guardamos
-                $userSend['name']           =$name;
-                $userSend['lastName']       =$last_name;
+                $userSend['emaila']           =$mail;
+                $userSend['pasahitza']        =$pasahitza;
             }
 
         }
