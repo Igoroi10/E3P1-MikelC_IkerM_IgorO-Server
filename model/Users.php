efad921f8aa6ffba58f865aa9ce1bdc2243d75bd
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-03-23 13:47:52 +0100

    [FIX] Users.php corrección de la información respecto a la tabla de la DB

--- model/Users.php
+++ model/Users.php
@@ -7,7 +7,7 @@ class User extends ModelBase
     function __construct()
     {
          //Inicializamos el nombre de la tabla
-         $this->table_name  = 'users';
+         $this->table_name  = 'erabiltzaileak';
          
         //Llamamos al constructor de la base ModelBase
         parent::__construct();
