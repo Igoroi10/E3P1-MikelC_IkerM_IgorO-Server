3b93738656db0be245d5d8e325efa063a0c92530
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-04-27 17:29:14 +0200

    [NEW] contraseña hasheada

--- routes/postNewPassword.php
+++ routes/postNewPassword.php
@@ -7,7 +7,7 @@ require_once (__DIR__."/../controller/Controller.php");
 
 //TEST OIST ROUTE QUITAR OISTERIORMENTE
 $_POST['emaila']          = 'iker.mendoza@ikasle.aeg.eus';
-$_POST['pasahitza']      = '1222222244';
+$_POST['pasahitza']      = '1234';
 
 
 if(isset($_POST['emaila']) && isset($_POST['pasahitza']))
@@ -17,8 +17,7 @@ if(isset($_POST['emaila']) && isset($_POST['pasahitza']))
     $pasahitza      = sanitizeString($_POST['pasahitza']);
     $userSend['error'] = "";
 
-    // echo $usuario;
-    // echo $pasahitza;
+    
     $confirmEmail = $user->getAllByColumn("emaila", $usuario);
 
     if(empty($confirmEmail))
@@ -27,16 +26,16 @@ if(isset($_POST['emaila']) && isset($_POST['pasahitza']))
     }
     else
     {
+        //hasheo de contraseña
+        $hash = password_hash($pasahitza, PASSWORD_DEFAULT);
+        $pasahitza = $hash;
+
         //Añadimos el nuevo objeto a la BD
         $returnValue = $user->alterPassword($usuario, $pasahitza);
         
         if($returnValue == FALSE)
         {
-            echo "Error en la in troduccion de nuevo elemento en la BD";
-        }
-        else
-        {
-            $userSend['error'] = "Change succesfully";
+            $userSend['error'] = "Changed succesfully";
         }
         
     }
