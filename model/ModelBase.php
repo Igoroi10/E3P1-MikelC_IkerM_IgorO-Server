3c674f0429081ff9e8a17133b99e90070d113944
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-04-27 14:00:15 +0200

    [NEW] countUsers in order to make the user_kod

--- model/ModelBase.php
+++ model/ModelBase.php
@@ -235,9 +235,23 @@ class ModelBase extends Conexion
     }
 }
 
+function comprobeIfMailExists($emaila, $pasahitza, $izen_abizena){
+
+}
+
+function countUsers($emaila, $pasahitza, $izen_abizena){
+    $query = "select count(user_kod) from erabiltzaileak;"
+    $result = $this->conexion->query($query);
+
+
+    $result->close();
+}
+
+}
+
 function insertUser($emaila, $pasahitza, $izen_abizena, $zenbakia){
 
-    $query = "insert into erabiltzaileak(user_kod,rol,izen_abizena,pasahitza,emaila) values (usr00$zenbakia, player, $izen_abizena, $emaila);";
+    $query = "insert into erabiltzaileak(user_kod,rol,izen_abizena,pasahitza,emaila) values (usr$zenbakia, player, $izen_abizena, $emaila);";
     $result = $this->conexion->query($query);
 
 
