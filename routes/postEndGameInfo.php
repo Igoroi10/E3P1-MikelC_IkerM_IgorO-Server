baa49b2a72fa9dcd2daa733ab2a2b7ba98efbc92
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-06-01 08:33:38 +0200

    [NEW] Creada file para post de End Game Info

---
+++ routes/postEndGameInfo.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once (__DIR__."/../controller/Controller.php");
+
+//TEST OIST ROUTE QUITAR OISTERIORMENTE
+//ejemplo del update en mysql -> update erabiltzaileak set rol = 'admin' where emaila = 'iker.mendoza@ikasle.aeg.eus';
+
+    // $_POST['newRola']          = "";
+    // $_POST['emaila']           = "igor.ocon@ikasle.aeg.eus";
+    // $_POST['newEmaila']        = "igoroi.ocon@ikasle.aeg.eus";   
+    // $_POST['newIzenAbizena']   = "Igor Ocon2";
+
+
+// Primer User
+if(isset($_POST['rola']) || isset($_POST['emaila']) || isset($_POST['izen_abizena']))
+{
+    $newRol         = $_POST['newRola'];
+    $mail           = $_POST['emaila'];
+    $newMail        = $_POST['newEmaila'];
+    $newName        = $_POST['newIzenAbizena'];
+    $userSend['error'] = "";
+
+    //Añadimos el nuevo objeto a la BD
+    $returnValue = $user-> updatePlayer($newRol, $mail, $newMail, $newName);
+
+    if($returnValue == FALSE)
+    {
+        $userSend['error'] =  "Usuario modificiado correctamente.";
+    }
+    echo json_encode($userSend);
+}
+else
+{
+    die("Forbidden");
+}
+
+
+// Segundo User
+if(isset($_POST['rola']) || isset($_POST['']) || isset($_POST['izen_abizena']))
+{
+    $newRol         = $_POST['newRola'];
+    $mail           = $_POST['emaila'];
+    $newMail        = $_POST['newEmaila'];
+    $newName        = $_POST['newIzenAbizena'];
+    $userSend['error'] = "";
+
+    //Añadimos el nuevo objeto a la BD
+    $returnValue = $user-> updatePlayer($newRol, $mail, $newMail, $newName);
+
+    if($returnValue == FALSE)
+    {
+        $userSend['error'] =  "Usuario modificiado correctamente.";
+    }
+    echo json_encode($userSend);
+}
+else
+{
+    die("Forbidden");
+}
+
+
+?>
